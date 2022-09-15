799d31e6009a33783c6b6517859a2128c9db9088
Mikhail <mikhail@MacBook-Air.local>
2022-09-16 00:09:42 +0300

    Task completed

---
+++ main.py
@@ -0,0 +1,124 @@
+import sys
+import math
+
+
+def get_coef(index, prompt):
+    """
+    Читаем коэффициент из командной строки или вводим с клавиатуры
+
+    Args:
+        index (int): Номер параметра в командной строке
+        prompt (str): Приглашение для ввода коэффицента
+
+    Returns:
+        float: Коэффициент биквадратного уравнения
+    """
+
+    try:
+        # Пробуем прочитать коэффициент из командной строки
+        coef_str = float(sys.argv[index])
+    except:
+        # Вводим с клавиатуры
+        print(prompt)
+        coef_str = input()
+
+        while type(coef_str) != float:
+            try:
+                coef_str = float(coef_str)
+            except:
+                print(prompt)
+                coef_str = input()
+
+    return coef_str
+
+
+def add_root(root_t, result, D_is_zero):
+    """
+    Добавление корней биквадратного уравнения
+
+    Args:
+        root_t (float): корень квадратного уравнение A*t^2 + B*t + C = 0
+        result (list[float]): список корней
+        D_is_zero (bool): флаг нулевого дискриминанта
+
+    Returns:
+        list[float]: Список корней
+    """
+
+    if root_t == 0:
+        result.append(0)
+
+    if D_is_zero:
+        if root_t > 0:
+            root = math.sqrt(root_t)
+            result.append(root)
+
+    else:
+        if root_t > 0:
+            root1 = math.sqrt(root_t)
+            result.append(root1)
+            result.append(-root1)
+
+    return result
+
+
+def get_roots(a, b, c):
+    """
+    Вычисление корней биквадратного уравнения
+
+    Args:
+        a (float): коэффициент А
+        b (float): коэффициент B
+        c (float): коэффициент C
+
+    Returns:
+        list[float]: Список корней
+    """
+
+    result = []
+
+    D = b * b - 4 * a * c
+
+    if D == 0.0:
+        root_t = -b / (2.0 * a)
+        result = add_root(root_t, result, D == 0.0)
+
+    elif D > 0.0:
+        sqD = math.sqrt(D)
+
+        root_t1 = (-b + sqD) / (2.0 * a)
+        root_t2 = (-b - sqD) / (2.0 * a)
+
+        result = add_root(root_t1, result, D == 0.0)
+        result = add_root(root_t2, result, D == 0.0)
+
+    return result
+
+
+def main():
+    """
+    Основная функция
+    """
+
+    a = get_coef(1, 'Введите коэффициент А:')
+    b = get_coef(2, 'Введите коэффициент B:')
+    c = get_coef(3, 'Введите коэффициент C:')
+    # Вычисление корней
+    roots = get_roots(a, b, c)
+    # Вывод корней
+    len_roots = len(roots)
+    if len_roots == 0:
+        print('Нет корней')
+    elif len_roots == 1:
+        print('Один корень: {}'.format(roots[0]))
+    elif len_roots == 2:
+        print('Два корня: {} и {}'.format(roots[0], roots[1]))
+    elif len_roots == 3:
+        print('Три корня: {}, {} и {}'.format(roots[0], roots[1], roots[2]))
+    elif len_roots == 4:
+        print('Четыре корня: {}, {}, {} и {}'.format(roots[0], roots[1], roots[2], roots[3]))
+
+
+# Если сценарий запущен из командной строки
+if __name__ == "__main__":
+    main()
